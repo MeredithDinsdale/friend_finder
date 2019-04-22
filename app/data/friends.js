1d6812db8117951090b500d118db2325df7960df
Meredith <dinsdale.meredith@gmail.com>
2019-04-22 09:19:04 -0700

    friend finder semi-functional

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,23 @@
 // ===============================================================================
 // DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
 // ===============================================================================
 
-var answersArray = [
+var friendsArray = [
     {
-      name: "Ahmed",
-      photo: "ahmed@example.com",
-      scores: []
+      name: "Rita",
+      photo: "https://thumbs-prod.si-cdn.com/X1ZibsBP5j3g6ccUN31rYco4_YM=/fit-in/1600x0/https://public-media.si-cdn.com/filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg",
+      scores: [5, 3, 2, 5, 4, 5, 4, 3, 1, 2]
+    },
+    {
+      name: "Simon",
+      photo: "https://thumbs-prod.si-cdn.com/X1ZibsBP5j3g6ccUN31rYco4_YM=/fit-in/1600x0/https://public-media.si-cdn.com/filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg",
+      scores: [1, 2, 4, 5, 3, 1, 3, 3, 4, 5]
+    },
+    {
+      name: "Dori",
+      photo: "https://www.enca.com/sites/default/files/chicken-head-2288234_1280.jpg",
+      scores: [2, 3, 1, 4, 2, 3, 3, 2, 1, 2]
     }
   ];
   
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = answersArray;
+  module.exports = friendsArray;
